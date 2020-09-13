6a4de60120b1044fb00a3730f6a3b560287417cc
MrHeer <hlm52pk@163.com>
2020-09-13 10:09:03 +0800

    feat: more style props

--- src/components/xnumber/Styled.tsx
+++ src/components/xnumber/Styled.tsx
@@ -2,7 +2,7 @@ import { animated } from 'react-spring'
 import styled from 'styled-components'
 import { IXNumberProps } from './interface'
 
-type StyleProps = Omit<IXNumberProps, 'value' | 'style'>
+type StyleProps = Omit<IXNumberProps, 'value' | 'style' | 'className'>
 
 export const XOneCharContainer = styled.div((_props) => ({
   overflowY: 'hidden'
@@ -13,13 +13,18 @@ export const StyledXOneChar = styled(animated.div)((_props) => ({
   willChange: 'transform, opacity'
 }))
 
-export const XNumberContainer = styled.div<StyleProps>((props) => ({
-  display: 'flex',
-  justifyContent: 'end',
-  alignItems: 'center',
-  color: props.color,
-  backgroundColor: props.backgroundColor,
-  fontSize: props.fontSize,
-  fontWeight: props.fontWeight,
-  padding: 5
-}))
+export const XNumberContainer = styled.div<StyleProps>((props) => {
+  const { width, height, color, backgroundColor, fontSize, fontWeight } = props
+  return {
+    display: 'flex',
+    justifyContent: 'end',
+    alignItems: 'center',
+    padding: 5,
+    width,
+    height,
+    color,
+    backgroundColor,
+    fontSize,
+    fontWeight
+  }
+})
